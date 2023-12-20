b0053c2ffacecaf3aaedf15ff8233a082c6866c3
DCKAP <poomikakdckap@gmail.com>
2023-12-20 13:16:32 +0530

    files added

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+let input1 = document.getElementById('product_form')
+let input2 = document.getElementById('amount_form')
+let btn = document.getElementById('btn')
+
+
+btn.addEventListener("click",but1)
+
+
+
+function but1(){
+ 
+
+  if(value==null){
+
+
+  if(input1.value=="" || input2.value=="")
+  {
+     alert("Please Enter a Valid Product")
+  }
+  else{
+   data1.innerText=input1.value;
+   input1.value="";
+   
+   data2.innerText=input2.value;
+   input2.value=""
+   
+   document.append(data1,data2)
+  }
+
+}
+  debtn.add
+}
